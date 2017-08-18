94eb7b8447e53749187ec1e4beefca771e056e8e
muleimulei <18729274217@163.com>
2017-08-18 23:51:20 +0800

    shadow

--- src/components/header/header.vue
+++ src/components/header/header.vue
@@ -13,23 +13,62 @@
           {{seller.description}}/{{seller.deliveryTime}}
         </div>
         <div v-if="seller.supports" class="support">
-          <span class="icon"></span>
+          <span class="icon" :class="classMap[seller.supports[0].type]"></span>
           <span class="text">{{seller.supports[0].description}}</span>
         </div>
+      </div>
+      <div v-if="seller.supports" class="support-count" @click="showDetail">
+        <span class="count">{{seller.supports.length}}个</span>
+        <i class="icon-keyboard_arrow_right"></i>
+      </div>
+    </div>
+    <div class="bulletin-wrapper" @click="showDetail">
+      <span class="bulletin-title"></span><span class="bulletin-text">{{seller.bulletin}}</span>
+      <i class="icon-keyboard_arrow_right"></i>
+    </div>
+    <div class="background">
+      <img :src="seller.avatar" width="100%" height="100%">
+    </div>
+    <div v-show="detailshow" class="detail">
+      <div class="detail-wrapper clearfix">
+        <div class="detail-main">
+          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Exercitationem et aspernatur, perferendis impedit sint nisi, a ipsam quibusdam id autem iste repudiandae fugit ullam mollitia deserunt vel ex doloribus totam.</p>
+          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nihil officia aut nobis, veritatis necessitatibus minus aliquam? Voluptatem provident labore quo, inventore, exercitationem assumenda, cumque dignissimos, a culpa minima voluptatibus cupiditate!</p>
+          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Exercitationem et aspernatur, perferendis impedit sint nisi, a ipsam quibusdam id autem iste repudiandae fugit ullam mollitia deserunt vel ex doloribus totam.</p>
+          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nihil officia aut nobis, veritatis necessitatibus minus aliquam? Voluptatem provident labore quo, inventore, exercitationem assumenda, cumque dignissimos, a culpa minima voluptatibus cupiditate!</p>
+          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Exercitationem et aspernatur, perferendis impedit sint nisi, a ipsam quibusdam id autem iste repudiandae fugit ullam mollitia deserunt vel ex doloribus totam.</p>
+          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nihil officia aut nobis, veritatis necessitatibus minus aliquam? Voluptatem provident labore quo, inventore, exercitationem assumenda, cumque dignissimos, a culpa minima voluptatibus cupiditate!</p>
+          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Exercitationem et aspernatur, perferendis impedit sint nisi, a ipsam quibusdam id autem iste repudiandae fugit ullam mollitia deserunt vel ex doloribus totam.</p>
+          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nihil officia aut nobis, veritatis necessitatibus minus aliquam? Voluptatem provident labore quo, inventore, exercitationem assumenda, cumque dignissimos, a culpa minima voluptatibus cupiditate!</p>
+        </div>
+      </div>
+      <div class="detail-close">
+        <i class="icon-close"></i>
       </div>
     </div>
   </div>
 </template>
 
 <script>
-export default{
-  name: 'head',
-  props: {
-    seller: {
-      type: Object
+  export default{
+    name: 'head',
+    props: {
+      seller: {
+        type: Object
+      }
+    },
+    methods: {
+      showDetail () {
+        this.detailshow = true
+      }
+    },
+    data () {
+      return {
+        detailshow: false,
+        classMap: ['decrease', 'discount', 'special', 'invoice', 'guarantee']
+      }
     }
   }
-}
 </script>
 
 <style lang='less'>
@@ -37,10 +76,12 @@ export default{
 
   .header{
     color: white;
-    background: #000;
+    position: relative;
+    background: rgba(7, 17, 27, 0.5);
     .content-wrapper{
       padding: 24px 12px 18px 24px;
       display: flex;
+      position: relative;
       .avatar > img{
         border-radius: 2px;
       }
@@ -53,7 +94,7 @@ export default{
             display: inline-block;
             width: 30px;
             height: 18px;
-            background-image: url('brand');
+            .bg-image('brand');
             background-size: cover;
           }
           .name{
@@ -69,6 +110,7 @@ export default{
           margin-bottom: 10px;
         }
         .support{
+          display: flex;
           .icon{
             display: inline-block;;
             width: 12px;
@@ -92,7 +134,92 @@ export default{
               .bg-image('special_1');
             }
           }
+          .text{
+            font-size: 12px;
+            line-height: 12px;
+          }
         }
+      }
+      .support-count{
+        position: absolute;
+        right: 12px;
+        bottom: 18px;
+        line-height: 24px;
+        padding: 0 8px;
+        border-radius: 14px;
+        background: rgba(0, 0, 0, .2);
+        text-align: center;
+        .count{
+          font-size: 10px;
+        }
+        .icon-keyboard_arrow_right{
+          margin-left: 2px;
+          line-height: 24px;
+          font-size: 10px;
+          display: inline-block;
+          vertical-align: bottom;
+        }
+      }
+    }
+    .bulletin-wrapper{
+      height: 28px;
+      line-height: 28px;
+      padding: 0 22px 0 12px;
+      white-space: nowrap;
+      overflow: hidden;
+      text-overflow: ellipsis;
+      position: relative;
+      background: rgba(7, 17, 27, 0.2);
+      .bulletin-title{
+        vertical-align: top;
+        display: inline-block;
+        margin-top: 9px;
+        width: 22px;
+        height: 12px;
+        .bg-image('bulletin');
+        background-size: cover;
+      }
+      .bulletin-text{
+        vertical-align: top;
+        font-size: 10px;
+        margin: 0 4px;
+      }
+      .icon-keyboard_arrow_right{
+        position: absolute;
+        font-size: 10px;
+        right: 12px;
+        top: 8px;
+      }
+    }
+    .background{
+      position: absolute;
+      width: 100%;
+      height: 100%;
+      top: 0;
+      left: 0;
+      z-index: -1;
+      filter: blur(10px)
+    }
+    .detail{
+      position: fixed;
+      top: 0;
+      left: 0;
+      width: 100%;
+      height: 100%;
+      background: rgba(7, 17, 27, 0.5);
+      z-index: 1;
+      .detail-wrapper{
+        min-height: 100%;
+        .detail-main{
+          margin-top: 64px;
+          padding-bottom: 64px;
+        }
+      }
+      .detail-close{
+        position: relative;
+        width: 32px;
+        height: 32px;
+        margin: -64px auto 0;
       }
     }
   }
